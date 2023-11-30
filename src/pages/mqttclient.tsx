0d59bfeb562f67aa2548985fbfaaa601ec3e43b9
adnanjpg <adnanfahedhr@gmail.com>
2023-12-01 00:18:57 +0300

    Refactor MQTTClient component to handle connection errors

--- src/pages/mqttclient.tsx
+++ src/pages/mqttclient.tsx
@@ -6,29 +6,37 @@ const MQTTClient: React.FC = () => {
   const [connectionStatus, setConnectionStatus] =
     useState<string>("Disconnected");
   const [channelInfo, setChannelInfo] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
-  let client: MqttClient | null = null;
+  const [client, setClient] = useState<MqttClient | null>(null);
 
   useEffect(() => {
     const connectToBroker = () => {
-      client = mqtt.connect(process.env.NEXT_PUBLIC_MQTT_BROKER_URL!, {
+      const cli = mqtt.connect(process.env.NEXT_PUBLIC_MQTT_BROKER_URL!, {
         username: process.env.NEXT_PUBLIC_MQTT_USERNAME!,
         password: process.env.NEXT_PUBLIC_MQTT_PASSWORD!,
       });
 
-      client.on("connect", () => {
+      cli.on("connect", () => {
         console.log("Connected to MQTT broker");
         setConnectionStatus("Connected");
         const topic = "your-own-topic"; // Replace with your desired topic/channel
-        client!.subscribe(topic);
+        cli.subscribe(topic);
         setChannelInfo(`Subscribed to ${topic}`);
+        setError(""); // Reset error on successful connection
       });
 
-      client.on("message", (topic, message) => {
+      cli.on("message", (topic, message) => {
         const incomingMessage = `Received message on topic ${topic}: ${message.toString()}`;
         console.log(incomingMessage);
         setMessages((prevMessages) => [...prevMessages, incomingMessage]);
       });
+
+      cli.on("error", (err) => {
+        setError(`MQTT client error: ${err.message}`);
+      });
+
+      setClient(cli);
     };
 
     connectToBroker();
@@ -38,6 +46,7 @@ const MQTTClient: React.FC = () => {
       setConnectionStatus("Disconnected");
       setChannelInfo("");
       setMessages([]);
+      setError("");
     };
   }, []);
 
@@ -45,6 +54,7 @@ const MQTTClient: React.FC = () => {
     if (client && client.connected) {
       client.publish(topic, payload);
     } else {
+      setError("MQTT client is not connected.");
       console.error("MQTT client is not connected.");
     }
   };
@@ -52,17 +62,30 @@ const MQTTClient: React.FC = () => {
   return (
     <div>
       <h1>MQTT Client</h1>
-      <p>Connection Status: {connectionStatus}</p>
-      <p>Channel Info: {channelInfo}</p>
-      <div>
+      <p className="rounded-md border-2 border-gray-500 p-2">
+        Connection Status: {connectionStatus}
+      </p>
+      <p className="rounded-md border-2 border-gray-500 p-2">
+        Channel Info: {channelInfo}
+      </p>
+      {/* box using tailwind */}
+      <div className="rounded-md border-2 border-gray-500 p-2">
         <h2>Incoming Messages</h2>
         <ul>
-          {messages.map((message, index) => (
-            <li key={index}>{message}</li>
-          ))}
+          {messages.length > 0
+            ? messages.map((message, index) => <li key={index}>{message}</li>)
+            : "No messages yet"}
         </ul>
       </div>
+      {error && (
+        <div>
+          <h2>Error</h2>
+          <p>{error}</p>
+        </div>
+      )}
+
       <button
+        className="rounded-md border-2 border-gray-500 p-2"
         onClick={() => sendMessage("your-own-topic", "Your message here")}
       >
         Send Message
